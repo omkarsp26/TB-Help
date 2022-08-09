180c7857686a8c4f32487fd229c08fec5fb5a39c
omkarsp26 <82698206+omkarsp26@users.noreply.github.com>
2022-08-10 00:03:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,26 +40,34 @@ def bot():
 # translator.translate("troubleshooting", dest="hi")   
     incoming_msg = incoming_msg.lower()
     
-    if 'hi' in incoming_msg:
+    if 'hi' in incoming_msg:    
         #out=translator.translate("troubleshooting", dest='hi')
-        output=(" This is a WhatsApp Messaging intervention for enhancing treatment adherence in Latent Tuberculosis Patients in Delhi \n\n Hello, Have you taken your medicine today \n type 1 for YES type 0 for NO? ")   
+        output=(" This is a WhatsApp Messaging intervention for enhancing treatment adherence in Latent Tuberculosis Patients in Delhi \n\n Hello, Have you taken your medicine today \n type 1 for YES type 0 for NO? ")
+    elif '1' in incoming_msg:
+        output =("Are you facing any difficulties due to medicine?")  
     else:
-        output = ("Bye")
+        output = ("Hello I am here to help you regarding your treatment")
         
         if '1' in incoming_msg:
             output = ("Are you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO? ")
         else:
-            output = ("Please take your medicines on time")
-            
-            if '1' in incoming_msg:
-                output = ("What difficulties are you facing? \n 1.Fever \n 2.unexplained anorexia \n 3.brown urine (colour of coffee or cola)")
-            else:
-                output = ("Have you contacted doctor? \n if yes type 1 and if no type 0")
-                
-                if '1' in incoming_msg:
-                    output = ("Have you received a call from your clinic?")
-                msg.body(output)
-                return str(resp) 
+            output = ("Please take your medications on time")
+        
+    msg.body(output)
+    return str(resp)
+    
+    
+ 
+    if '0' in incoming_msg:
+        output = ("Are you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO?")
+    else:
+        output = ("")
+        if '1' in incoming_msg:
+            output=("What difficulties are you facing? \n 1.Fever \n 2.unexplained anorexia \n 3.brown urine (colour of coffee or cola) ") 
+     
+ 
+    msg.body(output)
+    return str(resp) 
 
 if __name__ == "__main__":
 	app.run(debug=True)

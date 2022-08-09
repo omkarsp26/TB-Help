22c9ca6ba72a4fde12f9a20bad31f199053a84ab
omkarsp26 <82698206+omkarsp26@users.noreply.github.com>
2022-08-10 01:06:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def bot():
         output=(" This is a WhatsApp Messaging intervention for enhancing treatment adherence in Latent Tuberculosis Patients in Delhi \n\n Hello, Have you taken your medicine today \n type 1 for YES type 0 for NO? ")
        # incoming_msg = incoming_msg.lower()
         
-        if '1' in incoming_msg:
-            output = ("newAre you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO? ")
+        #if '1' in incoming_msg:
+           # output = ("newAre you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO? ")
             #incoming_msg = incoming_msg.lower()
             #if '1' in incoming_msg:
                # output=("What difficulties are you facing? \n 1.Fever \n 2.unexplained anorexia \n 3.brown urine (colour of coffee or cola) ")

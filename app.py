f611efb23276973043c0f0732465b705b1b5a6c0
omkarsp26 <82698206+omkarsp26@users.noreply.github.com>
2022-08-10 01:12:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,15 +42,14 @@ def bot():
     
     if 'hi' in incoming_msg:     
         output=(" This is a WhatsApp Messaging intervention for enhancing treatment adherence in Latent Tuberculosis Patients in Delhi \n\n Hello, Have you taken your medicine today \n type 1 for YES type 0 for NO? ")
+        incoming_msg = incoming_msg.lower()
         
-	incoming_msg = incoming_msg.lower()
-        
-        #if '1' in incoming_msg:
-           # output = ("newAre you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO? ")
+        if '1' in incoming_msg:
+            output = ("newAre you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO? ")
             #incoming_msg = incoming_msg.lower()
             #if '1' in incoming_msg:
                # output=("What difficulties are you facing? \n 1.Fever \n 2.unexplained anorexia \n 3.brown urine (colour of coffee or cola) ")
-        elif 'bulli' in incoming_msg:
+        if '0' in incoming_msg:
             output= ("hi")
         
         
@@ -64,28 +63,18 @@ def bot():
 
     
  
-    
+    if '0' in incoming_msg:
+        output = ("Are you facing any difficulties due to medicine? \n type 1 for YES type 0 for NO?")
+    else:
+        output = ("")
+        if '1' in incoming_msg:
+            output=("What difficulties are you facing? \n 1.Fever \n 2.unexplained anorexia \n 3.brown urine (colour of coffee or cola) ") 
      
  
-
+    msg.body(output)
+    return str(resp) 
 
 if __name__ == "__main__":
 	app.run(debug=True)
         
-    
-
-
-    
-
-    
-
-        
-    
-
-    
-
-    
-
-    
-
     
